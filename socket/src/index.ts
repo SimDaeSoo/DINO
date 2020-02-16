abd7e36ee90e92388b198774ef98ba5e943b29bd
SimDaeSoo <tlaeotn123@naver.com>
2020-02-17 08:35:31 +0900

    Master Server Port 변경.

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -4,7 +4,7 @@ async function start(): Promise<void> {
     const server: GameServer = new GameServer();
     await server.initialize();
     server.open(2000);
-    server.connectMaster(`localhost:1234`);
+    server.connectMaster(`localhost:3000`);
 }
 
 start();
