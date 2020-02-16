962dae2f44b7984bbb6b6bac3b32f723ec3b05ba
SimDaeSoo <tlaeotn123@naver.com>
2020-02-17 08:34:18 +0900

    Port 수정.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import MasterServer from '../../game/server/class/MasterServer';
 async function start(): Promise<void> {
     const server: MasterServer = new MasterServer();
     await server.initialize();
-    server.open(1234);
+    server.open(3000);
 }
 
 start();
--- socket/src/index.ts
+++ socket/src/index.ts
@@ -3,7 +3,7 @@ import GameServer from '../../game/server/class/GameServer';
 async function start(): Promise<void> {
     const server: GameServer = new GameServer();
     await server.initialize();
-    server.open(509);
+    server.open(2000);
     server.connectMaster(`localhost:1234`);
 }
 
