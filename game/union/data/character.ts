97b4efbf523e488dcca3ec784252008f5172058d
SimDaeSoo <tlaeotn123@naver.com>
2020-02-19 15:47:10 +0900

    Error 파악 못하겠다.. npm 버전업 하고 발생하는건가.

--- game/union/data/character.ts
+++ game/union/data/character.ts
@@ -10,13 +10,7 @@ const CharacterData: Dictionary<CharacterData> = {
     },
     2: {
         code: 2,
-        name: "다이노-그린",
-        description: "순한 다이노<br/>아군을 회복 한다.",
-        preview: "idle.gif"
-    },
-    3: {
-        code: 3,
-        name: "다이노-레드",
+        name: "다이노",
         description: "매서운 다이노<br/>빠른 파이어볼 공격을 한다.",
         preview: "idle.gif"
     },
