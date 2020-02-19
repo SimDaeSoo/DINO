ba83a1d4dd72550ac2750cabcc7089755ef2de43
SimDaeSoo <tlaeotn123@naver.com>
2020-02-19 15:54:06 +0900

    clone 다시 받으니 해결되는걸 보니.. 어떤문제인지 파악하기가 힘들다..

--- game/union/data/character.ts
+++ game/union/data/character.ts
@@ -10,7 +10,13 @@ const CharacterData: Dictionary<CharacterData> = {
     },
     2: {
         code: 2,
-        name: "다이노",
+        name: "다이노-그린",
+        description: "순한 다이노<br/>아군을 회복 한다.",
+        preview: "idle.gif"
+    },
+    3: {
+        code: 3,
+        name: "다이노-레드",
         description: "매서운 다이노<br/>빠른 파이어볼 공격을 한다.",
         preview: "idle.gif"
     },
